e8a131d2402d05b68f4bf82e601e7d27fe62e2ba
nami2012 <namithasadeeshbabu@gmail.com>
2020-08-03 19:28:33 +0530

    retrieve data from firebase

--- app.js
+++ app.js
@@ -3,31 +3,32 @@ const list = document.querySelector('#rescuerlist');
 function renderlist(doc){
     let li = document.createElement('li');
 
-    //let div = document.createElement('div');
-    //div.className = "item-data";
+    let div = document.createElement('div');
+    div.className = "item-data";
     
     let name = document.createElement('span');
     let age =  document.createElement('span');
     let contact = document.createElement('span');
     
     li.setAttribute('data-id',doc.id);
-    name.textContent = doc.data().name;
-    //name.className = "name-column";
-    age.textContent = doc.data().age;
-    //age.className = "age-column";
-    contact.textContent = doc.data().contact;
-    //contact.className = "contact-column";
+    name.textContent = doc.data().Name;
+    name.className = "name-column";
+    age.textContent = doc.data().Age;
+    age.className = "age-column";
+    contact.textContent = doc.data().Contact;
+    contact.className = "contact-column";
     
-    li.appendChild(name);
-    li.appendChild(age);
-    li.appendChild(contact);
-   
+    div.appendChild(name);
+    div.appendChild(age);
+    div.appendChild(contact);
+    li.appendChild(div);
     list.appendChild(li);
 }
 
 
 db.collection('Users').get().then((snapshot) =>{
     snapshot.docs.forEach(doc => {
+        console.log(doc.data());
         renderlist(doc);
     });
 
